2847893d49f393c23318d33d656d8c38412b1c4f
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-11-10 18:04:59 +0900

    main: implement lookup()

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,67 @@ use fuser::{Filesystem, MountOption};
 use libc::ENOENT;
 use log::*;
 use std::env;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
+use std::os::unix::prelude::*;
+use std::time::{Duration, SystemTime};
 
 struct GrpcFs {}
 
 impl Filesystem for GrpcFs {
-    fn getattr(&mut self, _req: &fuser::Request<'_>, _ino: u64, reply: fuser::ReplyAttr) {
+    fn init(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        _config: &mut fuser::KernelConfig,
+    ) -> Result<(), libc::c_int> {
+        Ok(())
+    }
+
+    /* fn getattr(&mut self, _req: &fuser::Request<'_>, _ino: u64, reply: fuser::ReplyAttr) {
         reply.error(ENOENT)
+    } */
+
+    fn lookup(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        _parent: u64,
+        name: &std::ffi::OsStr,
+        reply: fuser::ReplyEntry,
+    ) {
+        let default_duration = Duration::from_secs(1);
+
+        match fs::metadata(name) {
+            Ok(dentry_metadata) => {
+                let attr = |kind| fuser::FileAttr {
+                    ino: dentry_metadata.ino(),
+                    size: dentry_metadata.size(),
+                    blocks: dentry_metadata.blocks(),
+                    atime: SystemTime::UNIX_EPOCH,
+                    mtime: SystemTime::UNIX_EPOCH,
+                    ctime: SystemTime::UNIX_EPOCH,
+                    crtime: SystemTime::UNIX_EPOCH,
+                    kind: kind,
+                    perm: dentry_metadata.permissions().mode() as u16,
+                    nlink: dentry_metadata.nlink() as u32,
+                    uid: dentry_metadata.uid(),
+                    gid: dentry_metadata.gid(),
+                    rdev: dentry_metadata.rdev() as u32,
+                    blksize: dentry_metadata.blksize() as u32,
+                    flags: 0, // as I don't use this thing on macOS
+                };
+
+                if dentry_metadata.is_dir() {
+                    reply.entry(&default_duration, &attr(fuser::FileType::Directory), 0)
+                } else if dentry_metadata.is_file() {
+                    reply.entry(&default_duration, &attr(fuser::FileType::RegularFile), 0)
+                } else {
+                    reply.error(ENOENT);
+                }
+            }
+            Err(_) => {
+                reply.error(ENOENT);
+            }
+        };
     }
 
     fn readdir(
