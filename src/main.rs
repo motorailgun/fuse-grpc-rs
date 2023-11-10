7878425134b874cbad5129837a59d66c024c9bbe
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-11-10 16:34:05 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use fuser::{Filesystem, MountOption};
 use libc::ENOENT;
-use std::env;
 use log::*;
+use std::env;
 
 struct GrpcFs {}
 
@@ -11,13 +11,13 @@ impl Filesystem for GrpcFs {
     }
 
     fn readdir(
-            &mut self,
-            _req: &fuser::Request<'_>,
-            _ino: u64,
-            _fh: u64,
-            _ffset: i64,
-            reply: fuser::ReplyDirectory,
-        ) {
+        &mut self,
+        _req: &fuser::Request<'_>,
+        _ino: u64,
+        _fh: u64,
+        _ffset: i64,
+        reply: fuser::ReplyDirectory,
+    ) {
         reply.error(ENOENT)
     }
 
@@ -26,16 +26,16 @@ impl Filesystem for GrpcFs {
     }
 
     fn read(
-            &mut self,
-            _req: &fuser::Request<'_>,
-            _ino: u64,
-            _fh: u64,
-            _offset: i64,
-            _size: u32,
-            _flags: i32,
-            _lock_owner: Option<u64>,
-            reply: fuser::ReplyData,
-        ) {
+        &mut self,
+        _req: &fuser::Request<'_>,
+        _ino: u64,
+        _fh: u64,
+        _offset: i64,
+        _size: u32,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        reply: fuser::ReplyData,
+    ) {
         reply.error(ENOENT)
     }
 }
@@ -51,5 +51,5 @@ fn main() {
     // options.push(MountOption::AutoUnmount);
     // options.push(MountOption::AllowRoot);
 
-    fuser::mount2(GrpcFs{}, mountpoint, &options).unwrap();
+    fuser::mount2(GrpcFs {}, mountpoint, &options).unwrap();
 }
